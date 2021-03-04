bd390cd5b010cf753d10149850ae8e8f988cd1ad
Pablo Ferreira <pablo_ferreira14@hotmail.com>
2021-03-03 23:56:54 -0300

    Atualizado as dependências do projeto

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,77 @@
-require("dotenv").config()
+require('dotenv').config()
 
-const queries = require("./src/utils/algolia_queries")
+const queries = require('./src/utils/algolia_queries')
+
+const pluginConfig = [
+  `gatsby-plugin-transition-link`,
+  `gatsby-plugin-styled-components`,
+  `gatsby-plugin-react-helmet`,
+  // precisa ser o primeiro a trabalhar com gatsby-comment-images
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `uploads`,
+      path: `${__dirname}/static/assets/img`,
+    },
+  },
+  {
+    resolve: `gatsby-source-filesystem`,
+    options: {
+      name: `posts`,
+      path: `${__dirname}/posts`,
+    },
+  },
+  {
+    resolve: `gatsby-transformer-remark`,
+    options: {
+      plugins: [{
+        resolve: "gatsby-remark-relative-images",
+        options: {
+          name: "uploads"
+        }
+      },
+      {
+        resolve: "gatsby-remark-images",
+        options: {
+          maxWidth: 960,
+          linkImagesToOriginal: false
+        },
+      },
+        `gatsby-remark-lazy-load`,
+        `gatsby-remark-prismjs`,
+        `gatsby-remark-external-links`,
+      ],
+    },
+  },
+  `gatsby-transformer-sharp`,
+  `gatsby-plugin-sharp`,
+  {
+    resolve: `gatsby-plugin-algolia-search`,
+    options: {
+      appId: process.env.GATSBY_ALGOLIA_APP_ID,
+      apiKey: process.env.ALGOLIA_ADMIN_KEY,
+      indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
+      queries,
+      chunkSize: 10000,
+      enablePartialUpdates: true,
+    },
+  },
+  {
+    resolve: `gatsby-plugin-manifest`,
+    options: {
+      name: `Pablo Ferreira`,
+      short_name: `Pablo Ferreira`,
+      start_url: `/`,
+      background_color: `#121212`,
+      theme_color: `#121212`,
+      display: `minimal-ui`,
+      icon: `static/assets/img/favicon.png`,
+    },
+  },
+  `gatsby-plugin-sitemap`,
+  `gatsby-plugin-offline`,
+  `gatsby-plugin-netlify-cms`,
+]
 
 module.exports = {
   siteMetadata: {
@@ -10,74 +81,5 @@ module.exports = {
     author: `@Pablo_Ferreira`,
     siteUrl: `https://pabloferreira.netlify.app/`,
   },
-  plugins: [
-    `gatsby-plugin-transition-link`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-plugin-react-helmet`,
-    // precisa ser o primeiro a trabalhar com gatsby-comment-images
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `uploads`,
-        path: `${__dirname}/static/assets/img`,
-      },
-    },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `posts`,
-        path: `${__dirname}/posts`,
-      },
-    },
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [{
-          resolve: "gatsby-remark-relative-images",
-          options: {
-            name: "uploads"
-          }
-        },
-        {
-          resolve: "gatsby-remark-images",
-          options: {
-            maxWidth: 960,
-            linkImagesToOriginal: false
-          },
-        },
-          `gatsby-remark-lazy-load`,
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-external-links`,
-        ],
-      },
-    },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-algolia-search`,
-      options: {
-        appId: process.env.GATSBY_ALGOLIA_APP_ID,
-        apiKey: process.env.ALGOLIA_ADMIN_KEY,
-        indexName: process.env.GATSBY_ALGOLIA_INDEX_NAME,
-        queries,
-        chunkSize: 10000, // default: 1000
-        enablePartialUpdates: true,
-      },
-    },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Pablo Ferreira`,
-        short_name: `Pablo Ferreira`,
-        start_url: `/`,
-        background_color: `#121212`,
-        theme_color: `#121212`,
-        display: `minimal-ui`,
-        icon: `static/assets/img/favicon.png`, // This path is relative to the root of the site.
-      },
-    },
-    `gatsby-plugin-sitemap`,
-    `gatsby-plugin-offline`,
-    `gatsby-plugin-netlify-cms`,
-  ],
+  plugins: pluginConfig,
 }
