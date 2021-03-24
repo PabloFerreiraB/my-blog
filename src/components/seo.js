70718840759a1d0cb61b8bd180a26de690372a44
pablo <pablo_ferreira14@hotmail.com>
2021-03-24 01:38:41 -0300

    changes in page 404 and page seo

--- src/components/seo.js
+++ src/components/seo.js
@@ -24,7 +24,7 @@ function SEO({ description, lang, meta, title, image }) {
   const metaDescription = description || site.siteMetadata.description
 
   const url = site.siteMetadata.siteUrl
-  const ogImage = `${url}${image || "/assets/img/lake.jpg"}`
+  const ogImage = `${url}${image || "/assets/img/blog.png"}`
 
   return (
     <Helmet
