b00e0460c4d847bd2b26c73815a68ebc46bdd063
pablo <pablo_ferreira14@hotmail.com>
2021-03-24 23:41:39 -0300

    Upgrade blog to v3

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -1,24 +1,18 @@
 import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
+import { StaticImage } from "gatsby-plugin-image"
 
-import * as S from "./styled"
+import * as S from './styled'
 
 const Avatar = () => {
-  const { avatarImage } = useStaticQuery(
-    graphql`
-      query {
-        avatarImage: file(relativePath: { eq: "profile-photo.jpg" }) {
-          childImageSharp {
-            fluid(maxWidth: 60) {
-              ...GatsbyImageSharpFluid_tracedSVG
-            }
-          }
-        }
-      }
-    `
+  return(
+    <S.AvatarWrapper>
+      <StaticImage  
+        src="../../../static/assets/img/profile-photo.jpg"
+        alt="Pablo Avatar"
+        placeholder="blurred" 
+      />
+    </S.AvatarWrapper>
   )
-
-  return <S.AvatarWrapper fluid={avatarImage.childImageSharp.fluid} />
 }
 
 export default Avatar
--- src/components/MenuBar/index.js
+++ src/components/MenuBar/index.js
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from "react"
 import { Home } from "@styled-icons/boxicons-solid/Home"
 import { SearchAlt2 as Search } from "@styled-icons/boxicons-regular/SearchAlt2"
 import { UpArrowAlt as Arrow } from "@styled-icons/boxicons-regular/UpArrowAlt"
-import { Bulb as Light } from "@styled-icons/boxicons-regular/Bulb"
+import { Lightbulb as Light } from "@styled-icons/material-outlined/Lightbulb"
 import { Grid } from "@styled-icons/boxicons-solid/Grid"
 import { ListUl as List } from "@styled-icons/fa-solid/ListUl"
 import { Menu } from '@styled-icons/boxicons-regular/Menu'
